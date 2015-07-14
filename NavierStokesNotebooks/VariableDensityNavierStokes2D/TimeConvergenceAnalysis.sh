04c2b1e5679e854fa75d3cdc747e0d10e44e282a
Spencer Patty <srobertp@gmail.com>
2015-07-14 09:37:52 -0500

    fix last step of Time Convergence shell to reflect dt = 0.00625 not 0.006125

--- NavierStokesNotebooks/VariableDensityNavierStokes2D/TimeConvergenceAnalysis.sh
+++ NavierStokesNotebooks/VariableDensityNavierStokes2D/TimeConvergenceAnalysis.sh
@@ -4,19 +4,19 @@
 # to distinguish between the running locations
 
 
-sed 's/DT = 0\.[0-9]*/DT = 0.1/g' bdf1_variabledensity.py > tmp.py && mv -f tmp.py bdf1_variabledensity.py 
-parun bdf1_variabledensity_so.py -l5 -G -b L2_batch.py -D bdf1_dt_0_100000_results > dt_0_100000.out
+sed 's/DT = 0\.[0-9]*/DT = 0.1/g' navierstokes_vardensity.py > tmp.py && mv -f tmp.py navierstokes_vardensity.py 
+parun navierstokes_vardensity_so.py -l5 -G -b L2_batch.py -D navierstokes_dt_0_100000_results > dt_0_100000.out
 
-sed 's/DT = 0\.[0-9]*/DT = 0.05/g' bdf1_variabledensity.py > tmp.py && mv -f tmp.py bdf1_variabledensity.py 
-parun bdf1_variabledensity_so.py -l5 -G -b L2_batch.py -D bdf1_dt_0_050000_results > dt_0_050000.out
+sed 's/DT = 0\.[0-9]*/DT = 0.05/g' navierstokes_vardensity.py > tmp.py && mv -f tmp.py navierstokes_vardensity.py 
+parun navierstokes_vardensity_so.py -l5 -G -b L2_batch.py -D navierstokes_dt_0_050000_results > dt_0_050000.out
 
-sed 's/DT = 0\.[0-9]*/DT = 0.025/g' bdf1_variabledensity.py > tmp.py && mv -f tmp.py bdf1_variabledensity.py 
-parun bdf1_variabledensity_so.py -l5 -G -b L2_batch.py -D bdf1_dt_0_025000_results > dt_0_025000.out
+sed 's/DT = 0\.[0-9]*/DT = 0.025/g' navierstokes_vardensity.py > tmp.py && mv -f tmp.py navierstokes_vardensity.py 
+parun navierstokes_vardensity_so.py -l5 -G -b L2_batch.py -D navierstokes_dt_0_025000_results > dt_0_025000.out
 
-sed 's/DT = 0\.[0-9]*/DT = 0.0125/g' bdf1_variabledensity.py > tmp.py && mv -f tmp.py bdf1_variabledensity.py 
-parun bdf1_variabledensity_so.py -l5 -G -b L2_batch.py -D bdf1_dt_0_012500_results > dt_0_012500.out
+sed 's/DT = 0\.[0-9]*/DT = 0.0125/g' navierstokes_vardensity.py > tmp.py && mv -f tmp.py navierstokes_vardensity.py 
+parun navierstokes_vardensity_so.py -l5 -G -b L2_batch.py -D navierstokes_dt_0_012500_results > dt_0_012500.out
 
-sed 's/DT = 0\.[0-9]*/DT = 0.006125/g' bdf1_variabledensity.py > tmp.py && mv -f tmp.py bdf1_variabledensity.py 
-parun bdf1_variabledensity_so.py -l5 -G -b L2_batch.py -D bdf1_dt_0_006250_results > dt_0_006250.out
+sed 's/DT = 0\.[0-9]*/DT = 0.00625/g' navierstokes_vardensity.py > tmp.py && mv -f tmp.py navierstokes_vardensity.py 
+parun navierstokes_vardensity_so.py -l5 -G -b L2_batch.py -D navierstokes_dt_0_006250_results > dt_0_006250.out
 
 
