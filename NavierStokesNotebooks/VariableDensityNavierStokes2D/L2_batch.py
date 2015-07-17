bbd3035a158b36290aa1a57e448b75b466489410
Spencer Patty <srobertp@gmail.com>
2015-07-17 09:18:46 -0500

    add Norm tracking to pressure increment

--- NavierStokesNotebooks/VariableDensityNavierStokes2D/L2_batch.py
+++ NavierStokesNotebooks/VariableDensityNavierStokes2D/L2_batch.py
@@ -30,21 +30,21 @@ simFlagsList[1]['dataDir']        = os.getcwd()+'/results_BDF%1d' %int(float(ctx
 simFlagsList[1]['storeQuantities']= ['simulationData','errorData'] #include errorData for mass bal
 simFlagsList[1]['storeTimes']     = ['Last']
 
-# # Pressure Increment
-# simFlagsList[2]['errorQuantities']=['u']
-# simFlagsList[2]['errorTypes']= ['numericalSolution'] #compute error in soln and glob. mass bal
-# simFlagsList[2]['errorNorms']= ['L2'] #compute L2 norm in space or H1 or ...
-# simFlagsList[2]['errorTimes']= ['All'] #'All', 'Last'
-# simFlagsList[2]['echo']=True
-# simFlagsList[2]['dataFile']       = simFlagsList[2]['simulationName'] + "_DT_0_%s_BDF%1d.db" %(ctx.DT_string,int(float(ctx.globalBDFTimeOrder)))
-# simFlagsList[2]['dataDir']        = os.getcwd()+'/results_BDF%1d' %int(float(ctx.globalBDFTimeOrder))
-# simFlagsList[2]['storeQuantities']= ['simulationData','errorData'] #include errorData for mass bal
-# simFlagsList[2]['storeTimes']     = ['Last']
+# Pressure Increment
+simFlagsList[2]['errorQuantities']=['u']
+simFlagsList[2]['errorTypes']= ['numericalSolution'] #compute error in soln and glob. mass bal
+simFlagsList[2]['errorNorms']= ['L2'] #compute L2 norm in space or H1 or ...
+simFlagsList[2]['errorTimes']= ['All'] #'All', 'Last'
+simFlagsList[2]['echo']=True
+simFlagsList[2]['dataFile']       = simFlagsList[2]['simulationName'] + "_DT_0_%s_BDF%1d.db" %(ctx.DT_string,int(float(ctx.globalBDFTimeOrder)))
+simFlagsList[2]['dataDir']        = os.getcwd()+'/results_BDF%1d' %int(float(ctx.globalBDFTimeOrder))
+simFlagsList[2]['storeQuantities']= ['simulationData','errorData'] #include errorData for mass bal
+simFlagsList[2]['storeTimes']     = ['Last']
 
 # Pressure
 simFlagsList[3]['errorQuantities']=['u']
 simFlagsList[3]['errorTypes']= ['numericalSolution'] #compute error in soln and glob. mass bal
-simFlagsList[3]['errorNorms']= ['L2'] #compute L2 norm in space or H1 or ...
+simFlagsList[3]['errorNorms']= ['L2','H1semi'] #compute L2 norm in space or H1 or ...
 simFlagsList[3]['errorTimes']= ['All'] #'All', 'Last'
 simFlagsList[3]['echo']=True
 simFlagsList[3]['dataFile']       = simFlagsList[3]['simulationName'] + "_DT_0_%s_BDF%1d.db" %(ctx.DT_string,int(float(ctx.globalBDFTimeOrder)))
