8c27a10d77954c6eef28a814505aa3b4c34147d1
Spencer Patty <srobertp@gmail.com>
2015-08-24 16:51:31 -0500

    test case for ASGS turned off to do bdf2 standard and rotational models

--- NavierStokesNotebooks/VariableDensityNavierStokes2D/navierstokes_vardensity.py
+++ NavierStokesNotebooks/VariableDensityNavierStokes2D/navierstokes_vardensity.py
@@ -21,7 +21,7 @@ quad_degree = 5  # exact for polynomials of this degree
 # Model Flags
 globalBDFTimeOrder = 2 # 1 or 2 for time integration algorithms
 useRotationalModel = True #  Standard vs Rotational models in pressure update
-useStabilityTerms = True  # stability terms in density and velocity models
+useStabilityTerms = True  # stability terms in density and velocity models (should always be True)
 useNonlinearAdvection = False # switches between extrapolated and fully nonlinear advection in velocity model
 useNumericalFluxEbqe = True # ebqe history manipulation use ebqe or numericalFlux.ebqe which is exact
 useDirichletPressureBC = False  # Dirichlet bc pressure or zeroMean pressure increment
@@ -32,7 +32,7 @@ useScaleUpTimeStepsBDF2 = False  # Time steps = [dt^2, 2dt^2, 4dt^2, ... dt, ...
 setFirstTimeStepValues = False # interpolate the first step as well as the 0th step from exact solutions
 usePressureExtrapolations = False # use p_star instead of p_last in velocity and pressure model
 useConservativePressureTerm = False # use < -pI, grad w>  instead of < grad p, w> in velocity update
-useASGS=True
+useASGS=False  # turn on/off Algebraic Subgrid Stabilization for velocity and density transport
 
 # Spatial Discretization  he = he_coeff*2*Pi/150.0
 he_coeff = 0.75 # default to match Guermond paper: 0.75
@@ -40,7 +40,7 @@ he_coeff = 0.75 # default to match Guermond paper: 0.75
 # setup time variables
 
 T = 10.0
-DT = 0.05  # target time step size
+DT = 0.1  # target time step size
 
 
 # setup tnList
